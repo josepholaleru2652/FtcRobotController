9ada3c39d167a1cbb35a4c860647de2f130fe1c4
josepholaleru2652 <jos3ph.olaleru@gmail.com>
2025-11-08 08:53:37 -0700

    Update OTOSFullAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSFullAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSFullAuto.java
@@ -11,8 +11,6 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-// Tutorial: https://docs.google.com/document/d/1hLlhlEqIOicfobUs0HxBQp0qJFio-bUea9mtKIdgB2U/edit?tab=t.0
-
 @Autonomous(name = "OTOS Full Auto (Stable)")
 public class OTOSFullAuto extends LinearOpMode {
 
@@ -20,19 +18,24 @@ public class OTOSFullAuto extends LinearOpMode {
     private SparkFunOTOS myOtos;
     private DcMotor leftFront, leftBack, rightFront, rightBack;
     private DcMotor intakeMotor, outtakeMotor;
-    private Servo storageServo;
+    private Servo storageServo, doorServo;
+
+    int currentIndex = 0;
 
     // --- Config ---
-    private double power = 0.01;
+    private int startPosition = 1;
     private final double[] servoPositions = {0.0, 0.33, 0.66};
-    private final double POSITION_TOLERANCE = 0.5; // inches
-    private final double HEADING_TOLERANCE = 1;    // degrees
+    private final double POSITION_TOLERANCE = 1.0; // inches
+    private final double HEADING_TOLERANCE = 2.0;    // degrees
+
+    private double lastDrumTime = 0;       // time when drum last moved
+    private final double DRUM_INTERVAL = 0.5; // seconds between positions
+    private boolean drumActive = false;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // ======================================
-        // === INIT & Configure OTOS (Step 3) === 
-        // ======================================
+        // === INIT & Configure OTOS === (Step 3)
         //initHardware();
         //configureOtos();
 
@@ -75,6 +78,12 @@ public class OTOSFullAuto extends LinearOpMode {
         //     sleep(500);
         // }
 
+        // ---- Drum Test (G) ----
+        // Uncomment to rotate drum  for 6 seconds then stoo
+        // drumActive = true;
+        // sleep(6000);
+        // drumActive = false;
+
         /*
         =================================================================
         = UNCOMMENT TO RUN FULL POSITION 1 AUTONOMOUS SEQUENCE (Step 5) =
@@ -89,10 +98,12 @@ public class OTOSFullAuto extends LinearOpMode {
 
         // Start intake while moving to pickup
         intakeMotor.setPower(1);
+        drumActive = true;
         driveTo(new SparkFunOTOS.Pose2D(-39, 28, -90), 0.5);
 
         // Move back to drop position
         driveTo(new SparkFunOTOS.Pose2D(16, 28, -90), 0.5);
+        drumActive = false;
         intakeMotor.setPower(0);
 
         // Face forward again
@@ -106,6 +117,11 @@ public class OTOSFullAuto extends LinearOpMode {
 
         // Outtake sequence
         rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
 
         setDrivetrainPower(0, 0, 0);
         telemetry.addLine("AUTO COMPLETE.");
@@ -113,6 +129,125 @@ public class OTOSFullAuto extends LinearOpMode {
         sleep(1000);
 
          */
+        /*
+        =================================================================
+        = UNCOMMENT TO RUN FULL POSITION 2 AUTONOMOUS SEQUENCE (Step 6) =
+        =================================================================
+
+        // === AUTONOMOUS SEQUENCE (POS #2) ===
+        // Move forward 28 inches
+        driveTo(new SparkFunOTOS.Pose2D(0, 28, 0), 0.5);
+
+        // Turn right 90°
+        rotateTo(90, 0.3);
+
+        // Start intake while moving to pickup
+        intakeMotor.setPower(1);
+        drumActive = true;
+        driveTo(new SparkFunOTOS.Pose2D(39, 28, 90), 0.5);
+
+        // Move back to drop position
+        driveTo(new SparkFunOTOS.Pose2D(-16, 28, 90), 0.5);
+        drumActive = false;
+        intakeMotor.setPower(0);
+
+        // Face forward again
+        rotateTo(0, 0.3);
+
+        // Drive to backdrop
+        driveTo(new SparkFunOTOS.Pose2D(0, 75, 0), 0.5);
+
+        // Turn slightly to align for scoring
+        rotateTo(45, 0.3);
+
+        // Outtake sequence
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+
+        setDrivetrainPower(0, 0, 0);
+        telemetry.addLine("AUTO COMPLETE.");
+        telemetry.update();
+        sleep(1000);
+
+         */
+        /*
+        =================================================================
+        = UNCOMMENT TO RUN FULL POSITION 3 AUTONOMOUS SEQUENCE (Step 7) =
+        =================================================================
+
+        // === AUTONOMOUS SEQUENCE (POS #3) ===
+
+        driveTo(new SparkFunOTOS.Pose2D(53, 84, 126), 0.5);
+
+        rotateTo(-90, 0.3);
+
+        intakeMotor.setPower(1);
+        drumActive = true;
+        driveTo(new SparkFunOTOS.Pose2D(17, 84, 144), 0.5);
+
+        // Move back to drop position
+        driveTo(new SparkFunOTOS.Pose2D(72, 84, 144), 0.5);
+        drumActive = false;
+        intakeMotor.setPower(0);
+
+        rotateTo(-45, 0.3);
+
+        // Outtake sequence
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+
+        setDrivetrainPower(0, 0, 0);
+        telemetry.addLine("AUTO COMPLETE.");
+        telemetry.update();
+        sleep(1000);
+
+         */
+        /*
+        =================================================================
+        = UNCOMMENT TO RUN FULL POSITION 4 AUTONOMOUS SEQUENCE (Step 8) =
+        =================================================================
+
+        // === AUTONOMOUS SEQUENCE (POS #4) ===
+
+        driveTo(new SparkFunOTOS.Pose2D(91, 84, 217), 0.5);
+
+        rotateTo(90, 0.3);
+
+        intakeMotor.setPower(1);
+        drumActive = true;
+        driveTo(new SparkFunOTOS.Pose2D(127, 84, 144), 0.5);
+
+        // Move back to drop position
+        driveTo(new SparkFunOTOS.Pose2D(72, 84, 144), 0.5);
+        drumActive = false;
+        intakeMotor.setPower(0);
+
+        rotateTo(45, 0.3);
+
+        // Outtake sequence
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+        rampOuttakeAndScore();
+        drumSpin();
+
+        setDrivetrainPower(0, 0, 0);
+        telemetry.addLine("AUTO COMPLETE.");
+        telemetry.update();
+        sleep(1000);
+
+         */
+
+
     }
 
     // --- Initialization ---
@@ -122,12 +257,25 @@ public class OTOSFullAuto extends LinearOpMode {
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         outtakeMotor = hardwareMap.get(DcMotor.class, "outtakeMotor");
         outtakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         storageServo = hardwareMap.get(Servo.class, "storageServo");
-        storageServo.setPosition(servoPositions[0]);
+        storageServo.setPosition(0);
+
+        doorServo = hardwareMap.get(Servo.class, "doorServo");
+        doorServo.setPosition(0);
 
         myOtos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
     }
@@ -143,7 +291,16 @@ public class OTOSFullAuto extends LinearOpMode {
         myOtos.setAngularScalar(1.0);
         myOtos.calibrateImu();
         myOtos.resetTracking();
-        myOtos.setPosition(new SparkFunOTOS.Pose2D(0, 0, 0));
+        if (startPosition == 1) {
+            myOtos.setPosition(new SparkFunOTOS.Pose2D(0, 0, 0));
+        } else if (startPosition == 2) {
+            myOtos.setPosition(new SparkFunOTOS.Pose2D(0, 0, 0));
+        } else if (startPosition == 3) {
+            myOtos.setPosition(new SparkFunOTOS.Pose2D(26, 130, 126)); // field-relative position for pos 3
+        } else if (startPosition == 4) {
+            myOtos.setPosition(new SparkFunOTOS.Pose2D(118, 130, 217));// field-relative position for pos 4
+        }
+
 
         telemetry.addLine("OTOS configured!");
         telemetry.update();
@@ -161,6 +318,8 @@ public class OTOSFullAuto extends LinearOpMode {
             double powerY = clamp(errorY * 0.05, -maxPower, maxPower);
 
             setDrivetrainPower(powerX, powerY, 0);
+
+            drumSpinNonBlocking();
 
             telemetry.addData("DriveTo Target", "(%.1f, %.1f)", target.x, target.y);
             telemetry.addData("Current", "(%.1f, %.1f)", current.x, current.y);
@@ -188,6 +347,8 @@ public class OTOSFullAuto extends LinearOpMode {
     }
 
     private void rampOuttake() {
+        double power = 0.01;
+        power = 0.0;
         while(opModeIsActive() && power < 1.0) {
             power += 0.01;
             power = Range.clip(power, 0, 1);
@@ -200,6 +361,8 @@ public class OTOSFullAuto extends LinearOpMode {
     }
 
     private void rampOuttakeAndScore() {
+        double power = 0.01;
+        power = 0.0;
         while (opModeIsActive() && power < 1.0) {
             power += 0.01;
             power = Range.clip(power, 0.0, 1.0);
@@ -210,10 +373,32 @@ public class OTOSFullAuto extends LinearOpMode {
             sleep(20);
         }
 
-        storageServo.setPosition(servoPositions[2]); // drop item
+        doorServo.setPosition(1); // kick up item
         sleep(500);
-        storageServo.setPosition(servoPositions[0]); // reset servo
+        doorServo.setPosition(0); // reset servo
     }
+    private void drumSpin(){
+        currentIndex++;                       // move to next index
+        if (currentIndex >= servoPositions.length)  // if past end, loop back
+            currentIndex = 0;
+
+        storageServo.setPosition(servoPositions[currentIndex]);
+    }
+    private void drumStop(){
+        storageServo.setPosition(0);
+    }
+
+    private void drumSpinNonBlocking() {
+        if (!drumActive) return; // do nothing if drum is off
+
+        double currentTime = getRuntime();
+        if (currentTime - lastDrumTime >= DRUM_INTERVAL) {
+            currentIndex = (currentIndex + 1) % servoPositions.length;
+            storageServo.setPosition(servoPositions[currentIndex]);
+            lastDrumTime = currentTime;
+        }
+    }
+
 
     // --- Utilities ---
     private double distance(SparkFunOTOS.Pose2D a, SparkFunOTOS.Pose2D b) {
